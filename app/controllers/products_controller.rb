b9c69322b81484b56c51b7fc03e39ad5e3b71724
Abubakar Abdulai <abdulaiabubakar473@gmail.com>
2024-10-03 01:25:55 -0500

    Improved product layout: separate lines for title, price, stock, and category with styling

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,3 +1,10 @@
-def show
-  @product = Product.find(params[:id])
+# app/controllers/products_controller.rb
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
 end
