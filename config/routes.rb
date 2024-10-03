0de8e9271055ee73117919a57799c92a64282dcd
Abubakar Abdulai <abdulaiabubakar473@gmail.com>
2024-10-03 00:58:34 -0500

    Updated Products controller and index view

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  Rails.application.routes.draw do
+    resources :products, only: [:index]
+  end
+
 end
